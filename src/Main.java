0b2ab8e9df358894180d48edf872c460fa023344
Sebastian <sebastian.samuelsson@elev.ga.ntig.se>
2024-05-13 09:40:07 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.net.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        int portnumber = 8080;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        while (infinite) {
+            byte[] buf = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            System.out.println("Message received from client = "+msg);
+        }
+        serverMulticastSocket.close();
     }
 }
